871e66b7160191ccae0265ddb706638699e05b47
João Guilherme <joao.sczip@hotmail.com>
2020-07-23 18:45:21 -0300

    feat: add RemoteSendImage

--- src/data/usecases/send-image/remote-send-image.ts
+++ src/data/usecases/send-image/remote-send-image.ts
@@ -1,8 +1,32 @@
 import { SendImage } from "../../../domain/usecases/send-image";
 import { DetectedImage } from "../../../domain/models/detected-image";
+import { HttpPostClient } from "../../protocols/http/http-post-client";
 
-export class RemoteSendImage {
-  constructor() {}
+export class RemoteSendImage implements SendImage {
+  constructor(
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClient
+  ) {}
 
-  async send(params: SendImage.Params): Promise<void> {}
+  async send({ file, description }: SendImage.Params): Promise<DetectedImage> {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("description", description);
+
+    const httpResponse = await this.httpPostClient.post({
+      url: this.url,
+      body: formData,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    const { label, timestamp } = httpResponse.body;
+
+    return {
+      description,
+      label,
+      timestamp,
+    };
+  }
 }
