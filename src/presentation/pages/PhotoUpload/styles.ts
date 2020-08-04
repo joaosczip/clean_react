8678ca5485581a4067071dbe5a3e901a870ba952
João Guilherme <joao.sczip@hotmail.com>
2020-08-04 11:50:44 -0300

    chore: add Roboto font

--- src/presentation/pages/PhotoUpload/styles.ts
+++ src/presentation/pages/PhotoUpload/styles.ts
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  * {
+    font-family: "Roboto", sans-serif;
+  }
+`;
 
 export const FileInput = styled.input`
   display: none;
